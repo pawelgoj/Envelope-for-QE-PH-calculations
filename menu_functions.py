d5edfa76af3e2dab3a58d04f002487ca81891625
Pawel <71873238+pawelgoj@users.noreply.github.com>
2022-03-02 00:04:39 +0100

    Add menu functions

---
+++ menu_functions.py
@@ -0,0 +1,14 @@
+
+#TODO prepare this class 
+import webbrowser
+
+
+class MenuFunctions:
+    @staticmethod
+    def show_documentation():
+        webbrowser.open_new_tab("https://github.com/pawelgoj/Envelope-for-dynmat-quantum-esspreso"\
+            "/blob/main/README.md")
+
+    @staticmethod
+    def show_project_CDFFL():
+        webbrowser.open_new_tab("https://github.com/pawelgoj/CDFFL-Create-data-files-for-lammps-amorphous-simulations")
