42855407104c39bdd7c8cff332783787e8fc8589
Pawel <71873238+pawelgoj@users.noreply.github.com>
2022-03-05 01:11:47 +0100

    Cleaining

--- pruby.py
+++
@@ -1,8 +0,0 @@
-import os
-file_name = "D:/Praca/Symulacje/Narzędzia/Moje/Envelope_for_QE/Envelope-for-dynmat-quantum-esspreso"
-        
-
-        
-file_1 = os.path.join(file_name, 'AlPO4_Raman.png')
-            
-os.remove(file_1)
